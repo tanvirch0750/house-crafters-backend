435420564d5a5d197d06b8696b8fb4c88f50475f
tanvirch0750 <tanvir0750@yahoo.com>
2023-11-20 17:12:33 +0600

    changed production url

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ const app: Application = express();
 
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://house-crafters-frontend-ivory.vercel.app',
     credentials: true,
   })
 );
